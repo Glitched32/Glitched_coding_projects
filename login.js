ecb6ddca6f6e5156068282b0d4b9a8f496084887
Glitched32 <147268149+Glitched32@users.noreply.github.com>
2025-01-03 18:34:36 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+var password = getElementById="password"
+            if (password === "ADMINPASS"){
+                location.href='ADMIN.html'
+            }else{
+                function changetext(){
+                    document.getElementById("incorrect").innerHTML= "incorrect";
+                }
+            }
